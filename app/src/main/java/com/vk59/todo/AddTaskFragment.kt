da689ef929454549631b9eb6941e09e14288d783
ivan <jkostylev592@gmail.com>
2020-09-06 07:37:24 +0300

    Made blank for application.

--- app/src/main/java/com/vk59/todo/AddTaskFragment.kt
+++ app/src/main/java/com/vk59/todo/AddTaskFragment.kt
@@ -1,33 +1,63 @@
 package com.vk59.todo
 
 import android.os.Bundle
+import android.view.*
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.vk59.todo.database.Task
+import com.vk59.todo.databinding.FragmentAddTaskBinding
 
 class AddTaskFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private lateinit var taskViewModel: TaskViewModel
+    private lateinit var binding: FragmentAddTaskBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_add_task, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_add_task, container, false
+        )
+        setHasOptionsMenu(true)
+        binding.lifecycleOwner = this
+        taskViewModel = ViewModelProvider(this).get(TaskViewModel::class.java)
+
+        taskViewModel.eventTaskUpdate.observe(viewLifecycleOwner, {
+            if (it) {
+                taskViewModel.eventTaskAddFinished()
+                NavHostFragment.findNavController(this).navigate(
+                    R.id.action_addTaskFragment_to_taskListFragment
+                )
+            }
+        })
+        return binding.root
     }
 
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-                    AddTaskFragment().apply {
-                        arguments = Bundle().apply {
-                            putString(ARG_PARAM1, param1)
-                            putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.overflow_add_menu, menu)
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (item.itemId == R.id.save_task) {
+            taskViewModel.insertTask(
+                Task(
+                    binding.editTaskName.text.toString(),
+                    binding.editTaskDescription.text.toString(),
+                    Task.TASK_STATUS_NOT_FINISHED,
+                    binding.importanceSpinner.selectedItemPosition
+                )
+            )
+            true
+        } else {
+            NavigationUI.onNavDestinationSelected(
+                item,
+                NavHostFragment.findNavController(this)
+            ) || super.onOptionsItemSelected(item)
+        }
+    }
+
 }
--- app/src/main/java/com/vk59/todo/EditTaskFragment.kt
+++ app/src/main/java/com/vk59/todo/EditTaskFragment.kt
@@ -1,59 +1,66 @@
 package com.vk59.todo
 
 import android.os.Bundle
+import android.view.*
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.vk59.todo.databinding.FragmentEditTaskBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [EditTaskFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class EditTaskFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentEditTaskBinding
+    private lateinit var taskViewModel: TaskViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_task, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_edit_task, container, false
+        )
+        setHasOptionsMenu(true)
+        binding.lifecycleOwner = this
+        taskViewModel = ViewModelProvider(this).get(TaskViewModel::class.java)
+        binding.taskViewModel = taskViewModel
+
+        taskViewModel.task?.observe(viewLifecycleOwner, { task ->
+            if (task != null) {
+                binding.importanceSpinner.setSelection(task.taskImportance)
+            }
+        })
+
+        taskViewModel.eventTaskUpdate.observe(viewLifecycleOwner, {
+            if (it) {
+                taskViewModel.eventTaskUpdateFinished()
+                NavHostFragment.findNavController(this).navigate(
+                    R.id.action_editTaskFragment_to_taskListFragment
+                )
+            }
+        })
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment EditTaskFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            EditTaskFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.overflow_edit_menu, menu)
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (item.itemId == R.id.save_task) {
+            taskViewModel.task!!.value?.taskName = binding.editTaskName.text.toString()
+            taskViewModel.task!!.value?.taskDescription = binding.editTaskDescription.text.toString()
+            taskViewModel.task!!.value?.taskImportance = binding.importanceSpinner.selectedItemPosition
+            taskViewModel.updateTask()
+            true
+        } else {
+            NavigationUI.onNavDestinationSelected(
+                item,
+                NavHostFragment.findNavController(this)
+            ) || super.onOptionsItemSelected(item)
+        }
+    }
+
 }
